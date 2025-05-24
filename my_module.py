ced77a0b8d7b1edca9bec10eed8565d5db6cfb46
Nifemi Olawoye <213187210+Nife06@users.noreply.github.com>
2025-05-24 17:38:24 +0100

    Delete my_module.py

--- my_module.py
+++
@@ -1,8 +0,0 @@
-def add_numbera(a,b):
-  return a+b
-
-def count_vowels(text):
-  '''Return the number of vowels in a given text.'''
-  vowels = "aeiouAEIOU"
-  return sum(i for char in text if char in vowels)
-
